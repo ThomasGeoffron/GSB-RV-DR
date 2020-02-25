135716a2f3750a51bfe575b9354b17067c4d9ace
GEOFFRON <thomas.geoffron.sio@gmail.com>
2020-02-25 14:18:32 +0100

    Renommage package fr.gsb.rv.dr

--- GSB-RV-DR/src/fr/gsb/rv/dr/GSBRVDR.java
+++ GSB-RV-DR/src/fr/gsb/rv/dr/GSBRVDR.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gsb.rv.dr;
+package fr.gsb.rv.dr;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
