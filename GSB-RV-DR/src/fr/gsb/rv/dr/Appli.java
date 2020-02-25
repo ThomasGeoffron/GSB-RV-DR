1597289ee8cd26f720bf2ec366f4fbf3e0a9a391
GEOFFRON <thomas.geoffron.sio@gmail.com>
2020-02-25 14:41:09 +0100

    Création de la barre de menus

--- GSB-RV-DR/src/fr/gsb/rv/dr/Appli.java
+++ GSB-RV-DR/src/fr/gsb/rv/dr/Appli.java
@@ -10,6 +10,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -24,9 +27,18 @@ public class Appli extends Application {
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
         
+        MenuBar barreMenus = new MenuBar();
+           
+        Menu menuFichier = new Menu("Fichier");
+        MenuItem itemseConnecter = new MenuItem("Se connecter");
+
+        menuFichier.getItems().add(itemseConnecter);
+        
+        barreMenus.getMenus().add(menuFichier);
+        
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Authentification");
+        primaryStage.setTitle("GSB-RV-DR");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
