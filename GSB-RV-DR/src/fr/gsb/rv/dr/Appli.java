bb9f424fb2e730fb5f3ff4a34fff05cf32e27e63
GEOFFRON <thomas.geoffron.sio@gmail.com>
2020-02-25 14:30:54 +0100

    Modification des instructions de la méthode start()

--- GSB-RV-DR/src/fr/gsb/rv/dr/Appli.java
+++ GSB-RV-DR/src/fr/gsb/rv/dr/Appli.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,26 +18,15 @@ import javafx.stage.Stage;
  *
  * @author developpeur
  */
-public class GSBRVDR extends Application {
+public class Appli extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        BorderPane root = new BorderPane();
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Authentification");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
