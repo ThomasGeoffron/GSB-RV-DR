d4d67f79877a539fdd7af8f3b5aea0906935d2ac
GEOFFRON <thomas.geoffron.sio@gmail.com>
2020-03-03 14:49:09 +0100

    Implémentation classe Visiteur

--- GSB-RV-DR/src/fr/gsb/rv/dr/entites/Visiteur.java
+++ GSB-RV-DR/src/fr/gsb/rv/dr/entites/Visiteur.java
@@ -10,5 +10,44 @@ package fr.gsb.rv.dr.entites;
  * @author developpeur
  */
 public class Visiteur {
+    private String matricule;
+    private String nom;
+    private String prenom;
+
+    public Visiteur(String matricule, String nom, String prenom) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Visiteur{" + "matricule=" + matricule + ", nom=" + nom + ", prenom=" + prenom + '}';
+    }
+    
     
 }
