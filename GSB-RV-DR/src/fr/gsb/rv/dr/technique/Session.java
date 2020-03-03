486123d3299f0ed570eec25216b9c30e1d7f3003
GEOFFRON <thomas.geoffron.sio@gmail.com>
2020-03-03 15:09:14 +0100

    Implémentation classe Session

--- GSB-RV-DR/src/fr/gsb/rv/dr/technique/Session.java
+++ GSB-RV-DR/src/fr/gsb/rv/dr/technique/Session.java
@@ -5,10 +5,40 @@
  */
 package fr.gsb.rv.dr.technique;
 
+import fr.gsb.rv.dr.entites.Visiteur;
+
 /**
  *
  * @author developpeur
  */
 public class Session {
+    private static Session session;
+    private Visiteur leVisiteur;
     
+    private Session(Visiteur visiteur){
+        this.leVisiteur = visiteur;
+    }
+    
+    public static void ouvrir(Visiteur visiteur) {
+        Session.session = new Session(visiteur);
+    }
+    
+    public static void fermer() {
+        Session.session = null;
+    }
+    
+    public static Session getSession() {
+        return Session.session;
+    }
+    
+    public Visiteur getLeVisiteur() {
+        return this.leVisiteur;
+    }
+    
+    public static boolean estOuverte() {
+        if (Session.session == null) {
+            return false;
+        }
+        return true;
+    }
 }
